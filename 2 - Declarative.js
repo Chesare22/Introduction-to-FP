8fbc15c20411a6ceb5f4209c53604fa990dddb1e
César González <chesarglez429@gmail.com>
2021-07-27 19:48:38 -0500

    :bulb: Show how "map" would work if JS was functional

--- 2 - Declarative.js
+++ 2 - Declarative.js
@@ -2,6 +2,7 @@ const array = [1, 2, 3, 4, 5];
 const addOne = (x) => x + 1;
 
 const result = array.map(addOne);
+// result = map(addOne)(array)
 
 console.log("initial:", array);
 console.log("result: ", result);
