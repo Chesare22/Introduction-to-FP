e399f62cfa0e08c0b245efa2a977e2acbd409c55
César González <chesarglez429@gmail.com>
2021-07-27 22:27:37 -0500

    :recycle: Define range in a recursive way

--- naomiSolution.js
+++ naomiSolution.js
@@ -1,10 +1,10 @@
 const array = [1, 2, 3, 4, 5];
 const add = (y) => (x) => x + y;
 
-const range = (start) => (end) => {
-  const length = end - start + 1;
-  return Array.from({ length }, (_, i) => start + i);
-};
+const range = (start) => (end) =>
+  start === end
+    ? [start]
+    : [start, ...range(start + 1)(end)];
 
 const [addOne, addTwo, addThree] = range(1)(3).map(add);
 
